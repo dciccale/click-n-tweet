ca835b25025035901316912ab85cb0a51cafdd04
dciccale <dciccale@gmail.com>
2012-11-13 16:38:14 +0100

    reorganized files structure

--- clickntweet.js
+++
@@ -1,4 +0,0 @@
-chrome.tabs.getSelected(null, function (tab) {
-  var url = 'https://twitter.com/intent/tweet?url=' + encodeURI(tab.url) + '&text=' + encodeURIComponent(tab.title);
-  chrome.windows.create({ url: url, width: 550, height: 295, type: 'panel', focused: true });
-});
