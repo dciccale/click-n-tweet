93aae5bd571098e84c4fd1907e56dec7a95d1056
Denis Ciccale <dciccale@gmail.com>
2013-05-16 04:54:11 +0200

    add auto-close window feature and center the window nicely

--- src/clickntweet.js
+++ src/clickntweet.js
@@ -1,4 +1,17 @@
-chrome.tabs.getSelected(null, function (tab) {
-  var url = 'https://twitter.com/intent/tweet?url=' + encodeURI(tab.url) + '&text=' + encodeURIComponent(tab.title) + '&related=tdecs';
-  chrome.windows.create({ url: url, width: 550, height: 295, type: 'panel', focused: true });
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.tabs.getSelected(null, function (tab) {
+    var url = 'https://twitter.com/intent/tweet?url=' + encodeURI(tab.url) + '&text=' + encodeURIComponent(tab.title) + '&related=tdecs';
+    var width = 550;
+    var left = (screen.width/2)-(width/2);
+    chrome.windows.create({url: url, width: width, height: 290, top: 150, left: left, type: 'panel', focused: true}, function (window) {
+      var _tabId = window.tabs[0].id;
+      chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        if (tabId === _tabId) {
+          if (changeInfo.status === 'complete' && /complete/.test(tab.url)) {
+            chrome.windows.remove(window.id);
+          }
+        }
+      });
+    });
+  });
 });
