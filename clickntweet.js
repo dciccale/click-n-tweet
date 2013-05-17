9dd8208b4abc294dd57e539e5ccba3f82d44bb09
Denis Ciccale <dciccale@gmail.com>
2013-05-17 04:00:57 +0200

    remove related, as the window will autoclose

--- clickntweet.js
+++ clickntweet.js
@@ -1,13 +1,13 @@
 /*!
- * Click n' Tweet v1.4.6
+ * Click n' Tweet v1.4.7
  * https://chrome.google.com/webstore/detail/click-n-tweet/gdepkkciedfadghaigjanfkcdedenpbh
  * Copyright (c) 2013 Denis Ciccale (@tdecs)
  * Released under the MIT license
  * https://github.com/dciccale/click-n-tweet/blob/master/LICENSE.txt
  */
 chrome.browserAction.onClicked.addListener(function (tab) {
-  var twitterUrl = 'https://twitter.com/intent/tweet?related=tdecs';
-  var url = twitterUrl + '&url=' + encodeURI(tab.url) + '&text=' + encodeURIComponent(tab.title);
+  var twitterUrl = 'https://twitter.com/intent/tweet';
+  var url = twitterUrl + '?url=' + encodeURI(tab.url) + '&text=' + encodeURIComponent(tab.title);
   var width = 550;
   var left = (screen.width/2)-(width/2);
   chrome.windows.create({url: url, width: width, height: 270, top: 150, left: left, type: 'panel', focused: true}, function (window) {
